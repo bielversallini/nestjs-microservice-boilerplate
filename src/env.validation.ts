2856d8efe2c0e0ff4e46c154f626c9896e07293e
Gabriel Barbosa <gabriel@versallini.com.br>
2021-09-13 22:37:48 -0300

    create a way to validate environment variables

---
+++ src/env.validation.ts
@@ -0,0 +1,33 @@
+import { plainToClass } from 'class-transformer';
+import { IsEnum, IsNumber, validateSync } from 'class-validator';
+
+enum Environment {
+  Development = 'development',
+  Test = 'test',
+  Staging = 'staging',
+  Production = 'production',
+}
+
+export class EnvironmentVariables {
+  @IsEnum(Environment)
+  NODE_ENV: Environment;
+
+  @IsNumber()
+  PORT: number;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToClass(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+}
--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,17 @@ import { fastifyHelmet } from 'fastify-helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-  
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
   app.register(fastifyCookie);
   app.register(fastifyCsrf);
   app.register(fastifyHelmet);
   app.enableCors();
 
   await app.listen(3000, '0.0.0.0');
-
 }
 
 bootstrap();
