835ec9006b251a9e6476c2ef1de8456e2249fbaf
Gabriel Barbosa <gabriel@versallini.com.br>
2021-09-16 19:42:21 -0300

    add global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
@@ -14,6 +15,7 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
+  app.useGlobalPipes(new ValidationPipe());
   app.register(fastifyCookie);
   app.register(fastifyCsrf);
   app.register(fastifyHelmet);
