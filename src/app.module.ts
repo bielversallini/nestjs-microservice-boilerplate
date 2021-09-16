f90a6208a15b8f4c9a6dd7d32259d77366abd441
Gabriel Barbosa <gabriel@versallini.com.br>
2021-09-16 18:56:39 -0300

    Remove `hello world`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { validate } from './env.validation';
 
 @Module({
@@ -10,7 +8,5 @@ import { validate } from './env.validation';
       validate,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
